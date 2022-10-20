1553ba9108587b3ac1a66a18bb6029e34a34be9e
Connor Newton <connor@ifthenelse.io>
2022-10-20 11:01:40 +0100

    Implement trie

--- pyword/test_trie.py
+++ pyword/test_trie.py
@@ -1,2 +1,48 @@
-def test_dummy() -> None:
-    ...
+from pytest import mark, param
+
+from .trie import Trie
+
+
+class TestTrie:
+    @mark.parametrize(
+        ["key"],
+        [
+            param("trie", id="unique"),
+            param("lolol", id="repeat"),
+        ],
+    )
+    def test_first(self, key: str) -> None:
+        tr = Trie()
+        assert len(tr) == 0
+        assert key not in tr
+
+        tr.add(key)
+        assert len(tr) == 1
+        assert key in tr
+        assert tr.size() == len(key)
+
+        tr.remove(key)
+        assert len(tr) == 0
+        assert key not in tr
+        assert tr.size() == len(key)
+
+    def test_branch(self) -> None:
+        keys = ("branch", "brunch")
+
+        tr = Trie()
+        assert len(tr) == 0
+
+        for i, key in enumerate(keys, start=1):
+            assert key not in tr
+            tr.add(key)
+            assert len(tr) == i
+
+        assert tr.size() == 10
+        assert set(tr) == set(keys)
+
+        for i, key in enumerate(keys, start=1):
+            assert key in tr
+            tr.remove(key)
+            assert len(tr) == len(keys) - i
+
+        assert tr.size() == 10
---
+++ pyword/trie.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from contextlib import suppress
+from typing import Dict, Iterator, Mapping, MutableSet, Tuple
+
+
+class Trie(MutableSet[str]):
+    def __init__(self):
+        self.ok = False
+        self._next: Dict[str, Trie] = {}
+
+    @property
+    def next(self) -> Mapping[str, Trie]:
+        return self._next
+
+    def node(self, key: str, create: bool = False) -> Trie:
+        u = self
+        for i, c in enumerate(key):
+            try:
+                v = u._next[c]
+            except KeyError:
+                if not create:
+                    raise KeyError(f"{key}[{i}] not found")
+                v = Trie()
+                u._next[c] = v
+            u = v
+        return u
+
+    def add(self, key: str) -> None:
+        self.node(key, create=True).ok = True
+
+    def discard(self, key: str) -> None:
+        with suppress(KeyError):
+            self.node(key).ok = False
+
+    def contains(self, key: str) -> bool:
+        if not isinstance(key, str):
+            return False
+        try:
+            return self.node(key).ok
+        except KeyError:
+            return False
+
+    def nodes(self) -> Iterator[Tuple[str, Trie]]:
+        s = list(self.next.items())
+        while s:
+            wu = s.pop()
+            yield wu
+            w, u = wu
+            for c, v in u.next.items():
+                s.append((w + c, v))
+
+    def size(self) -> int:
+        n = 0
+        for _ in self.nodes():
+            n += 1
+        return n
+
+    def keys(self) -> Iterator[str]:
+        for w, n in self.nodes():
+            if n.ok:
+                yield w
+
+    def len(self) -> int:
+        n = 0
+        for _ in self.keys():
+            n += 1
+        return n
+
+    def __contains__(self, key: object) -> bool:
+        return isinstance(key, str) and self.contains(key)
+
+    __iter__ = keys
+    __len__ = len
